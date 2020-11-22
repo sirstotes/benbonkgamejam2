a4b35ec4bea7affaf232a06ceed740e8336cfee6
Dynamism <dhhhoyt2002@gmail.com>
2020-11-22 16:04:28 -0700

    Auto stash before merge of "master" and "origin/master"

--- Scripts/GameController.gd
+++ Scripts/GameController.gd
@@ -5,6 +5,11 @@ var itemsLeft
 var itemsInTruck = []
 var scene
 var vibing = 6.9
+var time = 0.0
+var trips = 0
+export(float) var time_cringe = 60.0
+export(float) var time_mutiplier = 0.5
+var default_score = 1000.0
 
 signal set_time(time)
 
@@ -12,10 +17,12 @@ func _ready():
 	$truck/Area.connect('body_entered', self, '_onItemEnteredTruck')
 	$truck/Area.connect('body_exited', self, '_onItemExitedTruck')
 func _process(delta):
+	time += 1
 	vibing += 0.01
 	vibing = clamp(vibing, 0, 7)
 	if Input.is_action_just_pressed("ui_focus_next") and vibing > 6.9:
 		vibing = 3
+		trips += 1
 	elif vibing < 6.9:
 		emit_signal("set_time", vibing)
 		if vibing > 4.99 and vibing < 5.01:
@@ -23,6 +30,11 @@ func _process(delta):
 	itemsLeft = $Objects.get_child_count()-len(itemsInTruck)
 	$CanvasLayer/Control/Label.text = "THERE ARE " + String(itemsLeft) + " ITEMS REMAINING"
 	if itemsLeft+len(itemsInTruck) <= 0:
+		var score = default_score
+		if time/60 > time_cringe * 60:
+			score = score/(((time/60)-time_cringe)*time_mutiplier)
+		score = score/trips
+		print(score)
 		_changeScene(nextLevel)
 	if scene != null:
 		transition.rect_position = transition.rect_position.linear_interpolate(Vector2(0, -4), 0.1)
