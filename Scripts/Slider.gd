046651abb3b2d94e4657e90f24210e1c6c67640c
Isaiah Stotesbery <sirstotes@gmail.com>
2020-11-22 12:09:59 -0700

    TItle Scene
    -Title Screen
    -Transitions
    -Truck Door Model

---
+++ Scripts/Slider.gd
@@ -0,0 +1,12 @@
+extends HSlider
+export var music = true
+func _ready():
+	if music:
+		value = GlobalMusic.get_node("Track1").volume_db
+	else:
+		pass#value = GlobalMusic.get_node("Track1").volume_db
+func _process(delta):
+	if music:
+		GlobalMusic.get_node("Track1").set_volume_db(value)
+	else:
+		pass#GlobalMusic.get_node("Track1").set_volume_db(value)
---
+++ Scripts/SoundController.gd
@@ -0,0 +1,16 @@
+extends Node
+var musicVolume : float = 0
+var soundVolume : float = 0
+func _ready():
+	pass
+#	$Music.play()
+#	$Music.connect("finished", $Timer, "start")
+#	$Timer.connect("timeout", $Music, "play")
+func _process(delta):
+	pass
+#	$Music.set_volume_db(musicVolume if musicVolume > -40 else -100)
+#	$Slide.set_volume_db(soundVolume if soundVolume > -40 else -100)
+#	$Completion.set_volume_db(soundVolume if soundVolume > -40 else -100)
+#	$Fill.set_volume_db(soundVolume if soundVolume > -40 else -100)
+#	$Move.set_volume_db(soundVolume if soundVolume > -40 else -100)
+#	$Start.set_volume_db(soundVolume if soundVolume > -40 else -100)
