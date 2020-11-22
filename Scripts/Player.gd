396799f3dc46f090cdcad4a6b3abc2ffbe75e4ed
Dyna <dhhoyt2002@gmail.com>
2020-11-21 18:55:34 -0700

    fdsa
    asdf

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -7,15 +7,20 @@ export(float) var jump_speed = 0
 export(float) var gravity = -50
 export(float) var sensitivity = 0.2
 export(float) var friction = 100
+export(NodePath) var crosshairPath
 
 var velocity = Vector3()
 var looking = Vector3()
 var holding = false
+var largeCrosshair = false
+var ready_to_let_down = false
 onready var viewport = $"Viewport"
 onready var pickup_node = $"Viewport/Pickup"
+var crosshair : TextureRect
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	crosshair = get_node_or_null(crosshairPath)
 	
 func _input(event):
 	if event is InputEventMouseMotion and !Input.is_action_pressed("player_rotate"):
@@ -24,15 +29,34 @@ func _input(event):
 		looking.x = clamp(looking.x, -PI/3, PI/3)
 		viewport.rotation.y = looking.y
 		viewport.rotation.x = looking.x
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+func _process(delta):
+	if crosshair != null:
+		if largeCrosshair:
+			if crosshair.rect_size.x < 10:
+				crosshair.rect_size.x += delta*50
+				crosshair.rect_size.y = crosshair.rect_size.x
+		else:
+			if crosshair.rect_size.x > 5:
+				crosshair.rect_size.x -= delta*50
+				crosshair.rect_size.y = crosshair.rect_size.x
 func _physics_process(delta):
 	velocity.y += gravity * delta
-	if Input.is_action_just_pressed("player_pickup"):
-		if !holding:
-			var space_state = get_world().direct_space_state
-			var result = space_state.intersect_ray(global_transform.origin, pickup_node.global_transform.origin, [self])
-			if len(result) > 0:
-				if result['collider'].is_in_group("Moving"):
-					result['collider'].ready_to_pick_up = true
+	if !holding:
+		var space_state = get_world().direct_space_state
+		var result = space_state.intersect_ray(global_transform.origin, pickup_node.global_transform.origin, [self])
+		if len(result) > 0 and result['collider'].is_in_group("Moving"):
+			largeCrosshair = true
+			if Input.is_action_just_pressed("player_pickup"):
+				result['collider'].ready_to_pick_up = true
+		else:
+			largeCrosshair = false
+	Input.is_action_just_pressed("player_pickup")
+	print(holding)
+	if ready_to_let_down:
+		holding = false
+		ready_to_let_down = false
 	if velocity.x > friction:
 		velocity.x -= friction
 	elif velocity.x < -friction:
@@ -71,6 +95,3 @@ func _physics_process(delta):
 		walkvector = walkvector.clamped(max_walk_speed)
 	velocity = Vector3(walkvector.x, velocity.y, -walkvector.y)
 	velocity = move_and_slide(velocity, Vector3(0, 1, 0), false, 4, 0.785398, false)
-	
-
-
